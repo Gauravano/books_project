740020130e1616e0fadfc9fa59dbd756ff1c95f3
gauravano <sachdeva.gaurav1997@gmail.com>
2018-07-01 14:07:05 +0530

    data models defined

--- db.js
+++ db.js
@@ -8,3 +8,75 @@ const db = new Sequelize('bookish', 'gaurav_bookish', 'Bookish 1', {
         max: 5
     }
 });
+
+const User = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    conatct_no: {
+        type: Sequelize.STRING,
+        validate: {
+            isNumeric: true,
+            is: ["^[0-9]+$", "i"]
+        }
+    },
+    address: {
+        type: Sequelize.STRING
+    },
+    college: {
+        type: Sequelize.STRING
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Listing = db.define('listings', {
+   id: {
+       type: Sequelize.INTEGER,
+       autoIncrement: true,
+       primaryKey: true
+   },
+   book_name: {
+       type: Sequelize.STRING,
+       allowNull: false,
+       unique: true
+   },
+   author_name: {
+       type: Sequelize.STRING,
+       allowNull: false
+   },
+    price: {
+       type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    condition: {
+       type: Sequelize.ENUM,
+       values: ['New', 'Almost New', 'Slight damage', 'Worn']
+    }
+});
+
+User.hasMany(Listing);
+// User.hasMany(Listing, {foreignKey: 'id', sourceKey: 'user_id'});
+
+db.sync().then(() => console.log("Database has been synced! "))
+         .catch((err) => console.log("Error creating database! "));
+
+exports = module.exports = {
+    User, Listing
+};
