0378a249ef9e7b5cb6928553742d91849c8d2847
gauravano <sachdeva.gaurav1997@gmail.com>
2018-07-05 21:48:12 +0530

    update 1

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -2,6 +2,64 @@ const route = require('express').Router();
 const Message = require('../../db').Message;
 const Listing = require('../../db').Listing;
 
+
+route.get('/:id', (req,res) => {
+  console.log("Messages");
+
+    if(!req.session.user){
+        return res.status(400).send("Please login to see the messages.");
+    }
+
+    // Listing.find({
+    //     where: {
+    //         id: req.params.listingId
+    //     }
+    // }).then((item) => {
+    //     if(item){
+    //         if(item.userId != req.session.user.id){
+    //             return res.status(401).send("Only book owner can see the messages!");
+    //         }
+    //     }else{
+    //         return res.status(400).send("Invalid listing!");
+    //     }
+    // })
+
+    Listing.find({
+      where: {
+        listingId: req.params.id
+      }
+    }).then((item) => {
+      if(req.session.user.id != item.userId) {
+          isSeller = false;
+      }else {
+          isSeller = true;
+      }
+    })
+
+    if(isSeller){
+      Message.findAll({
+          where: {
+              listingId: req.params.id
+          }
+      }).then((messages) => {
+          res.status(200).send(messages);
+      }).catch((err) => {
+          error: "Can't fetch messages!"
+      })
+  } else {
+    Message.findAll({
+        where: {
+            listingId: req.params.id,
+            senderId: req.session.user.id
+        }
+    }).then((messages) => {
+        res.status(200).send(messages);
+    }).catch((err) => {
+        error: "Can't fetch messages!"
+    })
+  }
+});
+
 route.post('/create', (req,res) => {
     if(!req.session.user){
         return res.status(400).send("Please login to send message to the book owner.");
@@ -21,34 +79,5 @@ route.post('/create', (req,res) => {
     })
 });
 
-route.get('/:listingId', (req,res) => {
-    if(!req.session.user){
-        return res.status(400).send("Please login to see the messages.");
-    }
-
-    Listing.find({
-        where: {
-            id: req.params.listingId
-        }
-    }).then((item) => {
-        if(item){
-            if(item.userId != req.session.user.id){
-                return res.status(401).send("Only book owner can see the messages!");
-            }
-        }else{
-            return res.status(400).send("Invalid listing!");
-        }
-    })
-
-    Message.findAll({
-        where: {
-            listingId: req.params.listingId
-        }
-    }).then((messages) => {
-        res.status(200).send(messages);
-    }).catch((err) => {
-        error: "Can't fetch messages!"
-    })
-});
 
 exports = module.exports = route;
